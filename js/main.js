66e7b9defd796a61b219a242ac67899ded508e84
junk1073 <junk1073@mail.ru>
2022-09-05 13:11:26 +0300

    start

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ header.classList.remove('header_active');
         }
     };
 }());
+
+// burger handler
+
+(function ()  {
+    const burgeritem = document.querySelector ('.burger');
+    const menu = document.querySelector ('.header__nav');
+    const menucloseitem = document.querySelector ('.header__nav-close');
+    burgeritem.addEventListener ('click', () => {
+        menu.classList.add ('header__nav_active');
+    });
+    menucloseitem.addEventListener ('click', () => {
+        menu.classList.remove ('header__nav_active');
+    })
+}());
